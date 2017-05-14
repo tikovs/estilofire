1180f6738784d7e19013cc0feb8e4f9eff47625d
Thiago <thiago.si@live.com>
2017-05-14 17:24:05 -0300

    working email with ajax and formspree

---
+++ _site/js/formspree.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    $('#contact-form').on('submit', function(e) {
+        e.preventDefault();
+        
+        //get the name field value
+        var name = $('#name').val();
+        //get the name field value
+        var email = $('#email').val();
+        //get the comments
+        var comments = $('#comments').val();
+                    
+        //pretend we don't need validation
+        
+        //send to formspree
+        $.ajax({
+            url:'https://formspree.io/tikovs@gmail.com',
+            method:'POST',
+            data:{
+                Nome:name,
+                 Email:email,
+                Mensagem:comments
+            },
+            dataType:"json",
+            success:function() {
+                $('#mail-message-area').show();
+            }   
+
+        });     
+        
+    });
+
+}); 

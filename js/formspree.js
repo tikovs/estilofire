477aa78c9b31e7e206d20375ecce2a92c1c52989
Thiago <thiago.si@live.com>
2017-05-14 17:31:05 -0300

    fix bugs

--- js/formspree.js
+++ js/formspree.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $('#contact-form').on('submit', function(e) {
         e.preventDefault();
-        
+          $btn = $("button#myButton").button('loading');
         //get the name field value
         var name = $('#name').val();
         //get the name field value
@@ -24,6 +24,7 @@ $(document).ready(function() {
             dataType:"json",
             success:function() {
                 $('#mail-message-area').show();
+                 $btn.button('reset');
             }   
 
         });     
